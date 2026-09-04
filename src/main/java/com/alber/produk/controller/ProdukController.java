57b132ae3b3111a2f1c66ef843b6232f661856a4
alberpamungkas <albertpamungkas597@gmail.com>
2026-09-04 10:57:41 +0700

    add entity: Produk

--- src/main/java/com/alber/produk/controller/ProdukController.java
+++ src/main/java/com/alber/produk/controller/ProdukController.java
@@ -1,23 +1,39 @@
 package com.alber.produk.controller;
 
+import com.alber.produk.entity.Produk;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/produk")
 public class ProdukController {
     @GetMapping
-    public String ping() {
-        return "ping";
+    public List<Produk> getAllProduk() {
+        List<Produk> produkList = new ArrayList<>();
+        produkList.add(new Produk(1, "Mouse", 100000.0, "merk 1"));
+        produkList.add(new Produk(2, "Keyboard", 200000.0, "merk 2"));
+        return produkList;
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Produk> getProdukById(@PathVariable("id") int id) {
+        if (id == 1) {
+            Produk produk = new Produk(1, "Mouse", 100000.0, "merk 1");
+            return ResponseEntity.ok(produk);
+        }
+        return ResponseEntity.notFound().build();
     }
 
-    @GetMapping("/{id}")
-    public String getProdukById(@PathVariable("id") int id) {
-        if (id == 1) {
-            return "Produk 1";
-        }
-        return "";
+    @PostMapping
+    public ResponseEntity<Produk> createProduk(@RequestBody Produk produk) {
+        // Implementation for creating a new produk
+        return ResponseEntity.ok(produk);
     }
 }
